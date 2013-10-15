c716b7dfea934b28304ff9ec129cc4d79932a024
mteaster <mteaster@uwm.edu>
2013-10-14 21:45:09 -0500

    unit testing project

--- band.Tests/Properties/AssemblyInfo.cs
+++ band.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("band")]
+[assembly: AssemblyTitle("band.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("band")]
+[assembly: AssemblyProduct("band.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a468b02b-db80-4acb-b6d7-0761777835dc")]
+[assembly: Guid("b2cb3077-d18c-46ab-8902-ecf8c1d851d8")]
 
 // Version information for an assembly consists of the following four values:
 //
